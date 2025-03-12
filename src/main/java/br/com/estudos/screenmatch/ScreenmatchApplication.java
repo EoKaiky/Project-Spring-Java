b6692c5ba32dba8ce91aea2b1ae02a24cd8872ca
EoKaiky <kaikybr234@gmail.com>
2025-03-12 13:20:59 -0300

    finalizando aula

--- src/main/java/br/com/estudos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/estudos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,7 @@
 package br.com.estudos.screenmatch;
+import br.com.estudos.screenmatch.model.DadosSerie;
 import br.com.estudos.screenmatch.service.ConsumoApi;
+import br.com.estudos.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +15,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=b370b0c");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=b370b0c");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.ObterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 
 }
--- src/main/java/br/com/estudos/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/estudos/screenmatch/model/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.estudos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String title,
                          @JsonAlias("totalSeasons") Integer totalSeasons,
                          @JsonAlias("imdbRating") String rating){
