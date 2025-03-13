62b816f43898e2fd62baf8f754eca0a7e11d34f4
EoKaiky <kaikybr234@gmail.com>
2025-03-13 13:06:01 -0300

    finalizando aula

---
+++ src/main/java/br/com/estudos/screenmatch/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.com.estudos.screenmatch.principal;
+
+import br.com.estudos.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=b370b0c";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para buscar: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+    }
+}
